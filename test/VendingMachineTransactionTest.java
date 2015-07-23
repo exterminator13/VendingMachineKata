7142cfdd2dbc7743a2f5439443b91bb32f1a1bbf
exterminator13 <launch35@gmail.com>
2015-07-23 19:07:17 -0400

    Split up the tests / made a @Before for use on multiple tests

--- test/VendingMachineTransactionTest.java
+++ test/VendingMachineTransactionTest.java
@@ -8,20 +8,28 @@ import static org.junit.Assert.*;
 import vendingmachinekata.VendingMachineTransaction;
 
 public class VendingMachineTransactionTest {
-    
+    VendingMachineTransaction test;
     public VendingMachineTransactionTest() {
-
+        
+    }
+    @Before
+    public void setUp(){
+        test = new VendingMachineTransaction();
     }
     @Test
     public void displaysInsertCoinWhileNotInTransaction(){
-        VendingMachineTransaction test = new VendingMachineTransaction();
         assertEquals("INSERT COIN", test.transactionTotalCoins());
     }
     @Test
     public void recognizesWeightAndDiameterOfQuater(){
-        VendingMachineTransaction test = new VendingMachineTransaction();
         assertEquals(25, test.coinRecognition("5.670 g", "0.955 in"));
+    }
+    @Test
+    public void recognizesWeightAndDiameterOfDime(){
         assertEquals(10, test.coinRecognition("2.268 g", "0.705 in"));
+    }
+    @Test
+    public void recognizesWeightAndDiameterOfNickel(){
         assertEquals(5, test.coinRecognition("5.000 g", "0.835 in"));
     }
 }
